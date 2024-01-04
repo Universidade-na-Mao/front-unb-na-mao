050034c8ff39f50b9b234ead2b76e2312ce04bda
LucasGSAntunes <190091681@aluno.unb.br>
2024-01-04 13:41:56 -0300

    Desenvolvimento inicial da tela de inicio

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,30 @@
 <template>
-  <q-page class="row items-center justify-evenly">
+  <q-page class="page">
     <example-component
       title="Example component"
       active
-      :todos="todos"
-      :meta="meta"
+      class="title row items-top justify-center"
     ></example-component>
+    <div class="row items-center justify-evenly">
+      <QCard class="card">
+        <q-icon name="dynamic_feed" size="100px"/>
+      </QCard>
+      <QCard class="card">
+        <q-icon name="restaurant_menu" size="100px"/>
+      </QCard>
+      <QCard class="card">
+        <q-icon name="send" size="100px"/>
+      </QCard>
+      <QCard class="card">
+        <q-icon name="event" size="100px"/>
+      </QCard>
+      <QCard class="card">
+        <q-icon name="school" size="100px"/>
+      </QCard>
+      <QCard class="card">
+        <q-icon name="settings" size="100px"/>
+      </QCard>
+    </div>
   </q-page>
 </template>
 
@@ -13,30 +32,28 @@
 import { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/ExampleComponent.vue';
 import { ref } from 'vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
 </script>
+
+<style scoped>
+  .page{
+    background-color: whitesmoke;
+  }
+  .title{
+    display: flex;
+  }
+
+  .card{
+    display: flex;
+    background: linear-gradient(0deg, #31813e 0%, #4e53ef 100%);
+    font-style: italic;
+    color: #ffffff;
+    justify-content: center;
+    justify-items: center;
+    min-width: 200px;
+    min-height: 100px;
+    cursor: pointer;
+    margin: 20px;
+    box-shadow: 5px 5px 10px 1px #4e53ef;
+    border-radius: 10px;
+  }
+</style>
